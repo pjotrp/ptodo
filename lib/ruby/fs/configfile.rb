1e2bd36dae296e6b8ce9926ded61c338e21920b3
Pjotr Prins <pjotr.public01@thebird.nl>
2017-03-01 09:51:54 +0000

    Show error when config file does not exist

--- lib/ruby/fs/configfile.rb
+++ lib/ruby/fs/configfile.rb
@@ -7,12 +7,12 @@
 class ConfigFile < Hash
 
   def initialize fn
-    return if not File.exist?(fn)
+    raise Exception.new("Config file #{fn} does not exist!") if not File.exist?(fn)
     File.new(fn).each_line do | line |
       next if line =~ /^#/ or line.strip =~ /^$/
       key,value = line.strip.split(/:/,2)
       self[key.strip] = value.strip
     end
   end
-  
+
 end
